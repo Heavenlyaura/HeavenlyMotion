5457446c15558911d80d44a8ce0f5a738a626983
heavenlyAura <goodnessokafor462@gmail.com>
2025-04-08 21:43:52 +0100

    Add SearchBar component and enhance main view layout with background image and scroll functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,29 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import SearchBar from "@/components/searchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text className="text-5xl text-accent font-bold">
-        Welcome to my App!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5 "
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
